dff0339f48f80794fe0fa80821b234a84b92ba62
twinh <twinhuang@qq.com>
2020-08-31 15:27:07 +0800

    refactor: 更改接口为 REST 风格
    BREAKING CHANGE: 更改接口为 REST 风格

--- CDeleteLink.js
+++ CDeleteLink.js
@@ -2,6 +2,6 @@ import React from 'react';
 import {DeleteLink} from '@mxjs/a-button';
 import curUrl from '@mxjs/cur-url';
 
-const CDeleteLink = ({id, ...props}) => <DeleteLink href={curUrl.apiDestroy(id)} {...props} />;
+const CDeleteLink = ({id, ...props}) => <DeleteLink href={curUrl.apiShow(id)} {...props} />;
 
 export default CDeleteLink;
