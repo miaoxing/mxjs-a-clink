73dd9593c97512be43ebbdbc39f76f97c372da34
twinh <twinhuang@qq.com>
2022-10-18 08:54:51 +0800

    refactor: export default 改为 export xx，以便 React fast refresh 生效

--- CNewBtn.js
+++ CNewBtn.js
@@ -1,6 +1,8 @@
 import {NewBtn} from '@mxjs/a-button';
 import curUrl from '@mxjs/cur-url';
 
-export default (props) => (
+const CNewBtn = (props) => (
   <NewBtn href={curUrl.new()} {...props}/>
 );
+
+export default CNewBtn;
