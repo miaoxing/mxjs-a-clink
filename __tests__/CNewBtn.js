1e811fbbc9abdc68057972efb128f26034145488
twinh <twinhuang@qq.com>
2023-05-31 23:02:25 +0800

    test(antd5): 关闭 `hashed`

--- __tests__/CNewBtn.js
+++ __tests__/CNewBtn.js
@@ -1,12 +1,17 @@
 import CNewBtn from '../CNewBtn';
 import {MemoryRouter} from 'react-router';
 import {render} from '@testing-library/react';
+import {ConfigProvider} from 'antd';
 
 describe('CNewBtn', () => {
   test('basic', () => {
-    const result = render(<MemoryRouter>
-      <CNewBtn/>
-    </MemoryRouter>);
+    const result = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter>
+          <CNewBtn/>
+        </MemoryRouter>
+      </ConfigProvider>
+    );
 
     expect(result.container).toMatchSnapshot();
   });
