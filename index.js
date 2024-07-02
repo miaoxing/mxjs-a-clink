79f4b9574da589d46f0adb6e9009f7d21a632264
twinh <twinhuang@qq.com>
2024-07-02 20:28:33 +0800

    fix: `export default from` 改为标准语法

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-export CDeleteLink from './CDeleteLink';
-export CEditLink from './CEditLink';
-export CListBtn from './CListBtn';
-export CNewBtn from './CNewBtn';
+import CDeleteLink from './CDeleteLink';
+import CEditLink from './CEditLink';
+import CListBtn from './CListBtn';
+import CNewBtn from './CNewBtn';
+
+export {
+  CDeleteLink,
+  CEditLink,
+  CListBtn,
+  CNewBtn,
+};
